078b32ee98e28341a64a9be9aaa45ec32db15785
stephensims03 <stephensims007@gmail.com>
2025-12-04 21:20:30 -0600

    Add files via upload

---
+++ testmodelcode12.py
@@ -0,0 +1,66 @@
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Install opencv-python
+import numpy as np
+import serial
+import time
+#openCV Keras tensorflow
+
+ser = serial.Serial('COM3', 9600, timeout=1) 
+time.sleep(2)
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    nameofclass = class_name.rstrip()
+    print(nameofclass)
+
+    #Tell Audino Code to turn open up garage
+    if nameofclass == "0 Car" and confidence_score >= .90:
+        print("There is Car")
+        ser.write(b'1')
+    else:
+        ser.write(b'0')
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+ser.close()
+camera.release()
+cv2.destroyAllWindows()
